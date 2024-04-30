5e93890601e56e321849603e6747aa84ae035740
Abhishek Kumar <ak35540@gmail.com>
2024-05-01 02:54:36 +0530

    commit

--- src/main/java/com/abhi/docker/basic_docker_app/BasicDockerAppApplication.java
+++ src/main/java/com/abhi/docker/basic_docker_app/BasicDockerAppApplication.java
@@ -8,7 +8,7 @@ public class BasicDockerAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicDockerAppApplication.class, args);
-		System.out.println("Hello World!");
+		System.out.println("Application Started");
 	}
 
 }
